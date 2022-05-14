1b8fbd222a247e72cb149cfb870255486fb40b40
BAM-hub <bsharamin12@gmail.com>
2022-05-14 20:47:51 +0300

    api requests added

--- screens/ScreenProvider.js
+++ screens/ScreenProvider.js
@@ -5,13 +5,16 @@ import {
 } from 'react-native';
 import LoadScreen from './LoadScreen';
 import RegisterScreen from './RegisterScreen';
-import GetLocation from 'react-native-get-location';
 import { io } from 'socket.io-client';
 import MapScreen from './MapScreen';
+import axios from 'axios';
 
-const SOCKET_URL = 'http://192.168.1.71:5000';
+const SOCKET_URL = 'http://192.168.1.71:8000';
+const API_KEY = 'AIzaSyCRqhSOtId37KlfnUoe8HYheJqbuzMn2Co';
 
 const ScreenProvider = () => {
+  const origin = { latitude: 31.966882, longitude: 35.988638 };
+  const destination = { latitude: 31.963817, longitude: 35.975449};
   const [markers, setMarkers] = useState([]);
   const [location, setLocation] = useState({
     longitude: 0,
@@ -24,53 +27,79 @@ const ScreenProvider = () => {
     id: ''
   });
 
-  
-  // useEffect(() => {
-  //   const _watchId = Geolocation.watchPosition(
-  //     position => {
-  //       const {latitude, longitude} = position.coords;
-  //       console.log({latitude, longitude});
-  //       setLocation({latitude, longitude});
-  //     },
-  //     error => {
-  //       console.log(error);
-  //     },
-  //     {
-  //       enableHighAccuracy: true,
-  //       distanceFilter: 0,
-  //       interval: 5000,
-  //       fastestInterval: 2000,
-  //     },
-  //   );
-  //   Geolocation.clearWatch(_watchId);
-  // }, []);
+
+
 
   // const [socket, setSocket] = useState(io(SOCKET_URL));
   // console.log(socket);
 
+  // useEffect(() => {
+  //   const socket = io(SOCKET_URL);
+  //   if(user.type !== '' && location.longitude !== 0 && location.latitude !== 0) {
+  //     console.log(user);
+  //     // console.log(socket);
+  //     // socket?.emit('join', {
+  //     //   ...user,
+  //     //   cords: location
+  //     // });
+  //   }
+  //   socket?.on('initial-coords', markerCords => {
+  //     console.log('markers: ', markerCords);
+  //       if(markers.length !== 0)
+  //         return;
+  //       setMarkers(markerCords);
+  //   });
+
+  //   return () => {
+  //     socket?.off('connection');
+  //     socket?.off('join');
+  //     socket?.close();
+  //   }
+  // }, [user.type, location.latitude, location.latitude]);
+
+  // useEffect(async () => {
+  //   try {
+  //     const config = {
+  //       method: 'get',
+  //       url: `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin.latitude},${origin.longitude}&destinations=${destination.latitude},${destination.longitude}&units=km&key=${API_KEY}`,
+  //       headers: {}
+  //     };
+  //     const res = await axios(config);
+  //     console.log(JSON.stringify(res.data));
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }, []);
   useEffect(() => {
-    const socket = io(SOCKET_URL);
+    const socket = io(SOCKET_URL, {
+      transports: ["websocket"]
+    });
     if(user.type !== '' && location.longitude !== 0 && location.latitude !== 0) {
-      console.log(user);
+      socket.connect();
       // console.log(socket);
-      socket?.emit('join', {
-        ...user,
-        cords: location
+      socket.on("connect", ()  => {
+        console.log('connected!');
+        socket.emit("my_event", { data: "do u hear me" });
       });
     }
+    socket?.emit('user_live',
+     {...user, cords: location}
+    );
     socket?.on('initial-coords', markerCords => {
       console.log('markers: ', markerCords);
         if(markers.length !== 0)
           return;
         setMarkers(markerCords);
     });
-
+    console.log(socket.connected);
+    console.log(markers);
     return () => {
       socket?.off('connection');
       socket?.off('join');
-      socket?.close();
+      socket.close();
     }
   }, [user.type, location.latitude, location.latitude]);
+
 
   return (
     <View style={styles.container}>
@@ -81,7 +110,7 @@ const ScreenProvider = () => {
     /> 
     : user.type === 'none' ? <RegisterScreen setUser={setUser} />
     : <MapScreen 
-        location={location}
+        location={origin}
         user={user}
         markers={markers}
       /> }

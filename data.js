bb0291661da64e71b0f7f46806219aa682680c49
BAM-hub <bsharamin12@gmail.com>
2022-05-16 02:19:08 +0300

    data for road lines might be deleted

---
+++ data.js
@@ -0,0 +1,58 @@
+export const drivers = [
+  {
+    id: 0,
+    name: 'Ali',
+    line: 'Amman As-Salt',
+    type: 'captin',
+    coords: {
+      latitude: '',
+      longitude: ''
+    }
+  }
+];
+
+export const passengers = [];
+
+export const landmarks = [
+  {
+    name: 'Al Adlyeh',
+    latitude: 31.956613,
+    longitude: 35.930945
+  },
+  {
+    latitude: 31.957059,
+    longitude: 35.930479
+  },
+  {
+    latitude: 31.958112,
+    longitude: 35.929449
+  },
+  {
+    latitude: 31.958576,
+    longitude: 35.928793
+  },
+  {
+    latitude: 31.958720,
+    longitude: 35.927967
+  }
+  // {
+  //   name: 'K.Hussein St. 250',
+  //   longitude: 31.969424,
+  //   latitude: 35.908760
+  // },
+  // {
+  //   name: 'Al Jamaa',
+  //   latitude: 32.015262,
+  //   longitude: 35.868275
+  // },
+  // {
+  //   name: 'Swellieh',
+  //   latitude: 32.022200,
+  //   longitude: 35.842673
+  // },
+  // {
+  //   name: 'As-Salt Station',
+  //   latitude: 32.034983,
+  //   longitude: 35.731041
+  // }
+];

ce2d4b9be7ecd844f3fdd82dfbb8a27e6cb8af44
BAM-hub <bsharamin12@gmail.com>
2022-05-26 23:10:53 +0300

    context added

--- App.js
+++ App.js
@@ -5,22 +5,25 @@ import { MarkersProvider } from './context/MarkersContext';
 import { SocketContext, socket } from './context/socket';
 import { UsertProvider } from './context/UserContext';
 import { HelperProvider } from './context/HelperContext';
+import { FocusLocationProvider } from './context/FocusLocationContext';
 import ScreenProvider from './screens/ScreenProvider';
 
 const App = () => {
   return (
     <SocketContext.Provider value={socket}>
-      <HelperProvider>
-      <UsertProvider>
-        <MarkersProvider>
-          <LocationProvider>
-            <DATAProvider>
-              <ScreenProvider />
-            </DATAProvider>
-          </LocationProvider>
-        </MarkersProvider>
-      </UsertProvider>
-      </HelperProvider>
+      <FocusLocationProvider>
+        <HelperProvider>
+          <UsertProvider>
+            <MarkersProvider>
+              <LocationProvider>
+                <DATAProvider>
+                  <ScreenProvider />
+                </DATAProvider>
+              </LocationProvider>
+            </MarkersProvider>
+          </UsertProvider>
+        </HelperProvider>
+      </FocusLocationProvider>
     </SocketContext.Provider>
   );
 };

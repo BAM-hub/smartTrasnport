f1964a5aafb1f081599aa5e1f5e99388e8197a0c
BAM-hub <bsharamin12@gmail.com>
2022-05-28 02:36:08 +0300

    now polling data from the server

--- context/DATAContext.js
+++ context/DATAContext.js
@@ -5,26 +5,26 @@ export const DATAContext = createContext();
 
 export const DATAProvider = (props) =>{
     const [DATA, setDATA] = useState([
-        {
-          name: 'BAM',
-          line: 'Amman As-Salt',
-          distance: 'Calculating',
-          time: 'Calculating',
-          coords: {
-            latitude: 31.966882,
-            longitude: 35.988638
-          }
-        },
-        {
-          name: 'Ali',
-          line: 'Amman As-Salt',
-          distance: 'Calculating',
-          time: 'Calculating',
-          coords: {
-            latitude: 31.963817,
-            longitude: 35.975449
-          }
-        },
+        // {
+        //   name: 'BAM',
+        //   line: 'Amman As-Salt',
+        //   distance: 'Calculating',
+        //   time: 'Calculating',
+        //   coords: {
+        //     latitude: 31.966882,
+        //     longitude: 35.988638
+        //   }
+        // },
+        // {
+        //   name: 'Ali',
+        //   line: 'Amman As-Salt',
+        //   distance: 'Calculating',
+        //   time: 'Calculating',
+        //   coords: {
+        //     latitude: 31.963817,
+        //     longitude: 35.975449
+        //   }
+        // },
       ]);
 
     return (

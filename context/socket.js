b5c73dffff7d31d38a56fe3986d5446d933ae340
BAM-hub <bsharamin12@gmail.com>
2022-05-29 03:07:17 +0300

    socket migrated to nodejs

--- context/socket.js
+++ context/socket.js
@@ -2,7 +2,5 @@ import { io } from "socket.io-client";
 import { SOCKET_URL } from "../config";
 import React from 'react';
 
-export const socket = io(SOCKET_URL, {
-    transports: ["websocket"]
-  });;
+export const socket = io(SOCKET_URL);;
 export const SocketContext = React.createContext(socket);
